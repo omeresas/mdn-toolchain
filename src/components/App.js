44c7627ec991a58add3a3492d5a72ecc3aa21979
omeresas <omer.esas@mail.polimi.it>
2021-07-30 11:16:34 +0200

    simple change 2

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ export default function App() {
     return acc;
   }, 0);
 
-  document.title = `${hazards} potential HAZARDS ${hazards > 0 ? '😱' : '👍'}`;
+  document.title = `${hazards} potential hazards ${hazards > 0 ? '😱' : '👍'}`;
 
   const results = data.result.near_earth_objects[day];
   return (
